30b38665c4c86bfc7f15500617aed73c8bf97c00
Dustin Harper <dustin.t.harper@utah.edu>
2024-03-15 13:27:59 -0400

    ts tweaks; coeff_mat_ice re-cal

--- phytoPSM_mucal.R
+++ phytoPSM_mucal.R
@@ -27,6 +27,17 @@ for (i in 1:length(iceco2.data)){
   iceco2.data[i] ~ dnorm(pco2[i], 1/(6)^2)
 }
 
+  # for (i in 1:length(mui.cd.data)){
+  #   mui.cd.data[i] ~ dnorm(mui.cd[i], 1/(1e-6)^2)
+  # }
+  # 
+  # for(i in 1:length(radius.cd)){
+  #   radius.cd[i] ~ dnorm(rm.cd[i], 1 / 0.25e-6^2)
+  # }
+  # 
+  # for(i in 1:length(po4.cd.data)){
+  #   po4.cd.data[i] ~ dnorm(po4.cd[i], 1 / 0.05^2)
+  # }
 ############################################################################################ 
 
 
@@ -69,7 +80,23 @@ mui.y.int ~ dnorm(y.int.lr, 1/(y.int.se.lr)^2)
 ############################################################################################ 
 
 
-# Proxy System Model 
+############################################################################################  
+# Proxy System Model - modern calibration data
+############################################################################################  
+# 
+# for (i in 1:length(radius.cd)){
+#   # Mean cell radius (m)
+#   rm.cd[i] ~ dnorm(rm.m, rm.p)T(0,5e-6)
+#   
+#   po4.cd[i] ~ dnorm(po4.m.cd, po4.p)T(0,2)
+# 
+#   # Calculate instantaneous growth rate (mu,i) from [PO4] and rmean
+#   mui.cd[i] <- coeff.po4*po4.cd[i] + coeff.rm*(rm.cd[i]*10^6) + mui.y.int
+# }
+############################################################################################ 
+
+
+# Proxy System Model - paleo data
 ############################################################################################
 for (i in 1:length(d13Cmarker.data)){
   # Calculate Uk'37 from temperature
